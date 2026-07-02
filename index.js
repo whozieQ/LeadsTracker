22d180b65bb06f725a2e2a7206c1909be54fe117
whozieQ <susieqz67@gmail.com>
2026-07-02 12:58:09 -0400

    initial firebase add

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js"
+
+const firebaseConfig = {}
+
+const app = initializeApp(firebaseConfig)
+console.log(app)
+
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
